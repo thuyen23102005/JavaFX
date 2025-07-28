5bf9654c4bc05f7fda1972d1d5893958f54abc83
Ngo Le Hoang Thuan <ngothuan1805@gmail.com>
2025-07-28 11:28:16 +0700

    m

--- src/Controller/LoginController.java
+++ src/Controller/LoginController.java
@@ -6,7 +6,14 @@ package Controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * FXML Controller class
@@ -15,9 +22,81 @@ import javafx.fxml.Initializable;
  */
 public class LoginController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private AnchorPane si_loginForm;
+    
+    @FXML
+    private TextField si_username;
+    
+    @FXML
+    private PasswordField si_password;
+    
+    @FXML
+    private Button si_loginBtn;
+    
+    @FXML
+    private Hyperlink si_forgotPass;
+    
+    @FXML
+    private AnchorPane su_signupForm;
+    
+    @FXML
+    private TextField su_username;
+    
+    @FXML
+    private PasswordField su_password;
+    
+    @FXML
+    private ComboBox<?> su_question;
+    
+    @FXML
+    private TextField su_answer;
+    
+    @FXML
+    private Button su_signupBtn;
+    
+    @FXML
+    private TextField fp_username;
+    
+    @FXML
+    private AnchorPane fp_questionForm;
+    
+    @FXML
+    private Button fp_proceedBtn;
+    
+    @FXML
+    private ComboBox<?> fp_question;
+    
+    @FXML
+    private TextField fp_answer;
+    
+    @FXML
+    private Button fp_back;
+    
+    @FXML
+    private AnchorPane np_newPassForm;
+    
+    @FXML
+    private PasswordField np_newPassword;
+    
+    @FXML
+    private PasswordField np_confirmPassword;
+    
+    @FXML
+    private Button np_changePassBtn;
+    
+    @FXML
+    private Button np_back;
+    
+    @FXML
+    private AnchorPane side_form;
+    
+    @FXML
+    private Button side_CreateBtn;
+    
+    @FXML
+    private Button side_alreadyHave;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
